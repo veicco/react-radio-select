233197745df94bfcf10bada7b3ff2b9c35637318
Veikko Kovanen <veikko.kovanen@gmail.com>
2018-02-11 18:01:48 +0200

    remove rest of the css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import RadioSelectRedux from "./redux/RadioSelectRedux";
 import RadioSelect from "./RadioSelect";
 import createReducer from "./redux/reducers";
-import "./style.scss";
 
 export {RadioSelect, RadioSelectRedux, createReducer};
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,4 @@
 const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-
 
 module.exports = {
   entry: {
@@ -19,19 +16,8 @@ module.exports = {
         use: ['babel-loader'],
         exclude: /node_modules/
       },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      }
     ],
   },
-  plugins: [
-    new ExtractTextPlugin('style.css'),
-    new CopyWebpackPlugin([{from: './src/style.scss', to: 'style.scss'}])
-  ],
   externals: {
     'react': 'commonjs react'
   }
